7d4f165fbf879bfd877286849ee12a030f03f181
Iván Rodríguez Ruiz <ivanrguez1@yahoo.es>
2025-04-05 13:28:12 +0200

    Añado formulario

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
+<!-- README.md -->
+
 # AnidiCursos
+
+
+## Comandos
+- symfony serve
+- php bin/console doctrine:database:drop --force
+- php bin/console doctrine:database:create
+- php bin/console make:migration
+- php bin/console doctrine:migrations:migrate
+- php bin/console make:entity
+
+## Endpoints
+- http://localhost:8000/
+
+### Endpoints de Inserción
+
+Aulas (tabla principal):
+- [C3] http://localhost:8000/crear-aulas -> array
+
+
+Cursos (tabla relacionada): 
+- [C4] http://localhost:8000/crea-curso/{expediente}/{denominacion}/{codAula}/{inicio}
+- http://localhost:8000/crea-curso/11A/MiCurso/1.1/2024-04-04
+
+### Endpoints de Consulta
+
+Aulas (tabla principal):
+- [R1] http://localhost:8000
+
+Cursos (tabla relacionada):
+- http://localhost:8000/consultar-cursos
+
+### Endpoints de Actualización
+
+- [U1] http://localhost:8000/cambiar-autor/{nif}/{nombre}/{edad}
+- [U2] http://localhost:8000/cambiar-articulo/{id}/{titulo}/{nifAutor}
+
+### Endpoints de Eliminación
+
+- [D1] http://localhost:8000/articulo-borrar/{id}
+
+### Endpoints de Formularios
+
+- [F1] http://localhost:8000/articulos-form
+- [F2] http://localhost:8000/autores-form
--- src/Controller/CursosController.php
+++ src/Controller/CursosController.php
@@ -7,8 +7,12 @@ use App\Entity\Cursos;
 use App\Repository\CursosRepository;
 use DateTimeInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -24,13 +28,17 @@ final class CursosController extends AbstractController
 
     // C4 -> Insertar 1 registro por parámetros
     // Variante C2. Controlamos dato tabla principal
-    #[Route('/crea-curso/{expediente}/{denominacion}/{codAula}/{inicio}',
+    #[Route(
+        '/crea-curso/{expediente}/{denominacion}/{codAula}/{inicio}',
         name: 'app_cursos_insertar_curso'
     )]
-    public function creaCurso(ManagerRegistry $doctrine,
-    string $expediente, string $denominacion, 
-    float $codAula, DateTimeInterface $inicio): Response
-    {
+    public function creaCurso(
+        ManagerRegistry $doctrine,
+        string $expediente,
+        string $denominacion,
+        float $codAula,
+        DateTimeInterface $inicio
+    ): Response {
         $entityManager = $doctrine->getManager();
         $nuevoCurso = new Cursos();
         $nuevoCurso->setExpediente($expediente);
@@ -38,10 +46,9 @@ final class CursosController extends AbstractController
         $nuevoCurso->setInicio($inicio);
 
         $mensaje = "";
-        $aula = $entityManager->getRepository
-        (Aulas::class)->find($codAula);
+        $aula = $entityManager->getRepository(Aulas::class)->find($codAula);
 
-        if($aula==null) {
+        if ($aula == null) {
             $mensaje = "ERROR! No existe el aula";
         } else {
             $nuevoCurso->setCodAula($aula);
@@ -54,8 +61,10 @@ final class CursosController extends AbstractController
 
 
     // R4 -> Consultar por parámetros. Salida Array JSON!
-    #[Route('/consultar-cursos', 
-    name: 'app_cursos_consultar_cursos')]
+    #[Route(
+        '/consultar-cursos',
+        name: 'app_cursos_consultar_cursos'
+    )]
     public function consultarCursos(CursosRepository $repo): JsonResponse
     {
         $cursos = $repo->findAll();
@@ -72,5 +81,48 @@ final class CursosController extends AbstractController
         }
 
         return new JsonResponse($miJSON);
+    }
+
+    // F1 -> Formulario completo Ambas tablas
+    #[Route('/cursos-form', name: 'app_cursos_form')]
+    public function cursosForm(ManagerRegistry $doctrine, Request $envio): Response
+    {
+        $curso = new Cursos();
+
+        $formulario = $this->createFormBuilder($curso)
+            ->add('expediente', TextType::class, [
+                'label' => 'Expediente',
+            ])
+            ->add('denominacion', TextType::class, [
+                'label' => 'Denominación',
+            ])
+            ->add('inicio', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Fecha Inicio',
+            ])
+            ->add('codAula', EntityType::class, [
+                'label' => 'Selecciona Aula',
+                'placeholder' => 'Elige una aula',
+                'class' => Aulas::class,
+                'choice_label' => function (Aulas $aula) {
+                    return sprintf('%s (Capacidad: %d)', $aula->getCodigo(), $aula->getCapacidad());
+                },
+            ])
+            ->getForm();
+
+        $formulario->handleRequest($envio);
+
+        if ($formulario->isSubmitted() && $formulario->isValid()) {
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($curso);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_cursos_consultar_cursos');
+        }
+
+        return $this->render('cursos/formulario.cursos.html.twig', [
+            'controller_name' => 'CursosController',
+            'formulario' => $formulario->createView(),
+        ]);
     }
 }
